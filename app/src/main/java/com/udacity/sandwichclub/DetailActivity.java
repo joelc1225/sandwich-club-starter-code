d443b2c69f65bad05b697cf018cefd111b980882
Joel Camargo <joelc1225@gmail.com>
2018-04-29 21:57:26 -0400

    1. adjust the detail activity layour to be cleaner 2. finish jsonUtils method to parse data 3. implement the populateUi method in detail activity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -2,8 +2,10 @@ package com.udacity.sandwichclub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -12,35 +14,38 @@ import com.udacity.sandwichclub.utils.JsonUtils;
 
 import org.json.JSONException;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private int mPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ImageView imageView = findViewById(R.id.image_iv);
 
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
-        if (position == DEFAULT_POSITION) {
+        mPosition = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        if (mPosition == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
             closeOnError();
             return;
         }
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
-        String json = sandwiches[position];
+        String json = sandwiches[mPosition];
         Sandwich sandwich = null;
         try {
-            sandwich = JsonUtils.parseSandwichJson(json);
+            sandwich = JsonUtils.parseSandwichJson(json, this);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -50,10 +55,12 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .error(R.drawable.placeholder2)
+                .placeholder(R.drawable.placeholder2)
+                .into(imageView);
 
         setTitle(sandwich.getMainName());
     }
@@ -63,7 +70,40 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
 
+        TextView akaTv = findViewById(R.id.akaTv);
+        TextView ingredientsTv = findViewById(R.id.ingredientsTv);
+        TextView placeOfOriginTv = findViewById(R.id.placeOfOriginTv);
+        TextView descriptionTv = findViewById(R.id.descriptionTv);
+
+        akaTv.setText(makeListIntoReadableString(sandwich.getAlsoKnownAs()));
+        ingredientsTv.setText(makeListIntoReadableString(sandwich.getIngredients()));
+        placeOfOriginTv.setText(sandwich.getPlaceOfOrigin());
+        descriptionTv.setText(sandwich.getDescription());
+    }
+
+    private String makeListIntoReadableString (List<String> list) {
+
+        StringBuilder finalString = new StringBuilder();
+
+        for (int i = 0; i < list.size(); i++) {
+
+            // checks if its the last position in list, so we dont add a comma
+            if (i == list.size() - 1) {
+                finalString.append(list.get(i));
+            } else {
+                finalString.append(list.get(i));
+                finalString.append(", ");
+            }
+        }
+
+        return finalString.toString();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putInt(getString(R.string.position_key), mPosition);
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,5 +1,9 @@
 package com.udacity.sandwichclub.utils;
 
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.udacity.sandwichclub.R;
 import com.udacity.sandwichclub.model.Sandwich;
 
 import org.json.JSONArray;
@@ -10,29 +14,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JsonUtils {
+    private static Resources resources;
 
-    public static Sandwich parseSandwichJson(String json) throws JSONException {
+    public static Sandwich parseSandwichJson(String json, Context context) throws JSONException {
 
         String mainName;
-        List<String> alsoKnownAs = null;
-        String placeOfOrigin = null;
-        String description = null;
-        String image = null;
-        List<String> ingredients = null;
+        List<String> alsoKnownAs;
+        String placeOfOrigin;
+        String description;
+        String image;
+        List<String> ingredients;
+        resources = context.getResources();
 
+        // creates base JSON object to retrieve all info
         JSONObject sandwichInfo = new JSONObject(json);
-        JSONObject sandiwchNameInfo = sandwichInfo.getJSONObject("name");
-        mainName = sandiwchNameInfo.getString("mainName");
+
+        // gets sandwich name info
+        JSONObject sandwichNameInfo = sandwichInfo.getJSONObject("name");
+        mainName = sandwichNameInfo.getString("mainName");
+        checkString(mainName);
+
         // gets the JSON array for aka names and parses that array to create List of strings
-        JSONArray akaJsonArray = sandiwchNameInfo.getJSONArray("alsoKnownAs");
+        JSONArray akaJsonArray = sandwichNameInfo.getJSONArray("alsoKnownAs");
         alsoKnownAs = parseJsonArray(akaJsonArray);
+
+        // gets place of origin and checks if valid
         placeOfOrigin = sandwichInfo.getString("placeOfOrigin");
+        checkString(placeOfOrigin);
+
+        // gets description and checks if valid
         description = sandwichInfo.getString("description");
+        checkString(description);
+
+        // gets image url. Picasso will handle null case, so i wont need to here
         image = sandwichInfo.getString("image");
+
+        // gets ingredients string array and parses into a List
         JSONArray ingredientsJsonArray = sandwichInfo.getJSONArray("ingredients");
         ingredients = parseJsonArray(ingredientsJsonArray);
 
-
+        // returns complete sandwich object
         return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
     }
 
@@ -41,12 +62,19 @@ public class JsonUtils {
 
         // checks if JSON array has any content before parsing
         if(jsonArray.length() == 0){
-            finishedList.add(0, "No data found");
+            finishedList.add(0, resources.getString(R.string.no_data_found));
         } else {
             for (int i = 0; i < jsonArray.length(); i++) {
                 finishedList.add( i, jsonArray.getString(i));
             }
         }
         return finishedList;
+    }
+
+    // check if valid string is present, if not changes string to error message
+    private static void checkString(String stringToCheck) {
+        if (stringToCheck.isEmpty()) {
+            stringToCheck = resources.getString(R.string.no_data_found);
+        }
     }
 }
